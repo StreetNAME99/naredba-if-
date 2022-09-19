62650303e152ad441baaf9b142b9c2886f472921
StreetNAME99 <113903197+StreetNAME99@users.noreply.github.com>
2022-09-19 14:37:10 +0200

    1 file modified

---
+++ index.js
@@ -0,0 +1,8 @@
+let a = prompt ("Unesite vase ime");
+console.log ("Uneli ste" + a + "!");
+let b = prompt ("Unesite vas broj");
+console.log = ("Uneli ste vas bro" + b + "!")
+let q
+if (true) {
+
+}
